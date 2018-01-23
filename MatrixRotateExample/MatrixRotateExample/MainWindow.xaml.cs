1e44f21c1f308dbfc9649b61989095b893cb0a4b
OXTS\HHAIDER <hhaider@oxts.com>
2018-01-23 15:17:38 +0000

    added project to test vectors

---
+++ MatrixRotateExample/MatrixRotateExample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatrixRotateExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //float imu_to_vehicle_heading   = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.DeviceToVehicleRotation.Imu2VehHeading.Value;
+            //float imu_to_vehicle_pitch     = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.DeviceToVehicleRotation.Imu2VehPitch.Value;
+            //float imu_to_vehicle_roll      = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.DeviceToVehicleRotation.InitialImu2VehRoll.Value;
+
+            //float lever_arm_x = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.PrimaryGNSSAntennaLeverArm.GAPx.Value;
+            //float lever_arm_y = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.PrimaryGNSSAntennaLeverArm.GAPy.Value;
+            //float lever_arm_z = (float) WarmUpPlugin.CurrentDevice.DeviceConfig.PrimaryGNSSAntennaLeverArm.GAPz.Value;
+
+            float imu_to_vehicle_heading   =   90f;
+            float imu_to_vehicle_pitch     =    0f;
+            float imu_to_vehicle_roll      =    0f;
+
+            float lever_arm_x =   1;
+            float lever_arm_y =  -1;
+            float lever_arm_z =  -1;
+
+            Vector3 lever_arm = new Vector3(lever_arm_x, lever_arm_y, lever_arm_z);
+
+            //Account for the difference in the OxTS HPR and dot NET Numerics
+            Matrix4x4 rotation = Matrix4x4.CreateFromYawPitchRoll(
+                                    (imu_to_vehicle_pitch   * (float)(Math.PI / 180)),
+                                    (imu_to_vehicle_roll    * (float)(Math.PI / 180)),
+                                    (imu_to_vehicle_heading * (float)(Math.PI / 180))
+                                    );
+             
+            Vector3 result_from_rotation   = Vector3.Transform(lever_arm, rotation);
+
+            //VehicleFramePrimaryLevelArmX = result_from_rotation.X;
+            //VehicleFramePrimaryLevelArmY = result_from_rotation.Y;
+            //VehicleFramePrimaryLevelArmZ = result_from_rotation.Z;
+
+            Oxts
+
+        }
+    }
+}
---
+++ WPFAnimations/AnimationTests/VectorDrawing/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VectorDrawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Line SpinnyLine { get; set; }
+        public List<Vector> Vectors { get; set; }
+        public ObservableCollection<Line> Lines {get; set;}
+ 
+        public double Center_X { get; set; }
+        public double Center_Y { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Center_X = 0;
+            Center_Y = 0;
+
+            //DrawLine();
+            //UpdateLine();
+
+
+            GenerateVectors();
+            DrawVectors();
+                    
+        }
+
+        public void DrawLine()
+        {
+            double Point_X  = Center_X + 50;
+            double Point_Y  = Center_Y;
+
+            // Add a Line Element
+            SpinnyLine = new Line
+            {
+                Stroke = System.Windows.Media.Brushes.LightSteelBlue,
+                X1 = Center_X,
+                Y1 = Center_Y,
+                X2 = Point_X,
+                Y2 = Point_Y,
+                StrokeThickness = 2
+            };
+
+            MainWindowCanvas.Children.Add(SpinnyLine);
+        }
+
+        public void UpdateLine()
+        {
+            Task.Run(() =>
+            {
+                Vector initial_position = new Vector();
+
+                while (true)
+                {
+                    Application.Current.Dispatcher.Invoke(() => {
+
+                        initial_position = new Vector
+                        {
+                            X = SpinnyLine.X2,
+                            Y = SpinnyLine.Y2
+                        };
+
+                    });
+
+
+                    Matrix rotation_matrix = new Matrix(
+                        Math.Cos(10 * Math.PI / 180),
+                       -Math.Sin(10 * Math.PI / 180),
+                        Math.Sin(10 * Math.PI / 180),
+                        Math.Cos(10 * Math.PI / 180),
+                        200, 
+                        200);
+
+
+
+                    Vector rotated_vector =  Vector.Multiply(initial_position, rotation_matrix);
+
+
+                    Application.Current.Dispatcher.Invoke(() => {
+
+
+                        SpinnyLine.X2 = rotated_vector.X;
+                        SpinnyLine.Y2 = rotated_vector.Y;
+
+
+
+                    });
+
+                    Thread.Sleep(500);
+                }
+            });
+            
+        }
+
+        public void GenerateVectors()
+        {
+            Vectors = new List<Vector>();
+            Random r = new Random();
+
+            for(int vector_count = 0; vector_count < 3; vector_count++) 
+            {
+                Vectors.Add(new Vector(r.Next(400,500), r.Next(400, 500)));
+            } 
+        }
+
+        public void DrawVectors()
+        {
+            Random r = new Random();
+            byte[] bytes = new byte[3];
+            Lines = new ObservableCollection<Line>();
+
+            foreach(Vector v in Vectors)
+            {
+                r.NextBytes(bytes);
+
+                Lines.Add(new Line
+                {
+                    X1 = Center_X,
+                    Y1 = Center_Y,
+                    X2 = v.X,
+                    Y2 = v.Y,
+                    Stroke =  new SolidColorBrush(Color.FromRgb(bytes[0], bytes[1], bytes[2])),
+                });
+
+                MainWindowCanvas.Children.Add(Lines[Lines.Count - 1]);
+
+            }
+
+            Vector average_vector = CalculateAverageVector();
+
+            Line average_line = new Line
+            {
+                X1 = Center_X,
+                Y1 = Center_Y,
+                X2 = average_vector.X,
+                Y2 = average_vector.Y,
+                Stroke = Brushes.Red,
+                StrokeThickness = 3,
+            };
+
+            MainWindowCanvas.Children.Add(average_line);
+
+        }
+
+        public Vector CalculateAverageVector()
+        {
+            Vector average_vector = new Vector();
+
+            foreach(Vector v in Vectors)
+            {
+                average_vector.X += v.X;
+                average_vector.Y += v.Y;
+            }
+
+            average_vector.X = average_vector.X / Vectors.Count;
+            average_vector.Y = average_vector.Y / Vectors.Count;
+
+            return average_vector;
+        }
+    }
+}
