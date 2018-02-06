7f865555f0e9edb4b8aa89917a18bf9f4e75a163
OXTS\HHAIDER <hhaider@oxts.com>
2018-02-06 12:19:54 +0000

    added in project for the resource manager

---
+++ WPFResourceManager/WPFResourceManager/WPFResourceManager/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFResourceManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Page> pages;
+        private int current_page_number;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pages = new List<Page>
+            {
+               new Page1(),
+               new Page2(),
+               new Page3(),
+            };
+
+            current_page_number = 0;
+
+            MainWindowFrame.Content = pages[current_page_number];
+
+        }
+
+        private void Next(object sender, RoutedEventArgs e)
+        {
+            if( current_page_number < (pages.Count - 1))
+            {
+                current_page_number++;
+            }
+
+            MainWindowFrame.Content = pages[current_page_number];
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            if (current_page_number != 0)
+            {
+                current_page_number--;
+            }
+
+            MainWindowFrame.Content = pages[current_page_number];
+        }
+
+    }
+}
---
+++ WPFResourceManager/WPFResourceManager/WPFResourceManager/SharedResources.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Reflection;
+
+namespace WPFResourceManager.Resources
+{
+    public static class SharedResources
+    {
+        #region Properties
+
+        public static readonly DependencyProperty MergedDictionariesProperty = DependencyProperty.RegisterAttached("MergedDictionaries",
+                                                                                                                    typeof(string), 
+                                                                                                                    typeof(SharedResources),
+                                                                                                                    new FrameworkPropertyMetadata((string)null,
+                                                                                                                    new PropertyChangedCallback(OnMergedDictionariesChanged)));
+
+
+
+        #endregion
+
+        #region Accessor Functions
+
+        public static string GetMergedDictionaries(DependencyObject d)
+        {
+            return (string)d.GetValue(MergedDictionariesProperty);
+        }
+
+        public static void SetMergedDictionaries(DependencyObject d, string value)
+        {
+            d.SetValue(MergedDictionariesProperty, value);
+        }
+
+        #endregion
+     
+        #region Members
+
+        private static Dictionary<string, WeakReference> m_shared_dictionaries = new Dictionary<string, WeakReference>();
+
+        #endregion
+
+        #region Methods
+
+        private static void OnMergedDictionariesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewValue as string))
+            {
+                foreach (string dictionary_name in (e.NewValue as string).Split(';'))
+                {
+                    ResourceDictionary dictionary = GetResourceDictionary(dictionary_name);
+
+                    if (dictionary != null)
+                    {
+                        if (d is FrameworkElement)
+                        {
+                            (d as FrameworkElement).Resources.MergedDictionaries.Add(dictionary);
+                        }
+                        else if (d is FrameworkContentElement)
+                        {
+                            (d as FrameworkContentElement).Resources.MergedDictionaries.Add(dictionary);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static ResourceDictionary GetResourceDictionary(string dictionary_name)
+        {
+            ResourceDictionary result = null;
+
+            if (m_shared_dictionaries.ContainsKey(dictionary_name))
+            {
+                result = (ResourceDictionary)m_shared_dictionaries[dictionary_name].Target;
+            }
+            if (result == null)
+            {
+                string assembly_name = System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().ManifestModule.Name);
+
+                result = Application.LoadComponent(new Uri(assembly_name + ";component/Resources/" + dictionary_name + ".xaml", UriKind.Relative)) as ResourceDictionary;
+
+                m_shared_dictionaries[dictionary_name] = new WeakReference(result);
+            }
+            return result;
+        }
+
+        #endregion
+
+    }
+}
